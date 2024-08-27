332cfcf14659f044f0b4a1d7bd4dc30e377fd354
tuyenbug <tuyen.nguyen5@ntq-solution.com.vn>
2024-08-27 16:10:43 +0700

    done

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,51 +1,80 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform, useWindowDimensions } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { ThemedTextInput } from '@/components/ThemedTextInput';
+import { ThemedButton } from '@/components/ThemedButton';
+import { useState } from 'react';
+
+type ObjectData = {
+  [key: string]: string
+}
+type FormData = ObjectData & {
+  userName?: string;
+  password?: string
+}
 
 export default function HomeScreen() {
+  const { width, height} = useWindowDimensions();
+  const [isLogin, setLogin] = useState(false);
+  const [form, setForm] = useState<FormData>({
+    userName: "",
+    password: ""
+  });
+  const onChangeText = (dataForm: FormData) => {
+    setForm(data => ({
+      ...data,
+      ...dataForm
+    }))
+  }
+
+  const onSubmit = () => {
+    for (let key in form) {
+      if (!form[key]) {
+        return;
+      }
+    }
+    setLogin(true)
+  }
+  if (isLogin) {
+    return (
+      <ThemedView style={{ justifyContent: 'center', alignItems: 'center', width: "100%", height: "100%"}}>
+        <ThemedText>
+          Login success!
+        </ThemedText>
+      </ThemedView>
+    )
+  }
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+          source={require('@/assets/images/logo.png')}
+          style={[styles.reactLogo, { width: width }]}
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Sign In</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
+        <ThemedText type="defaultSemiBold">UserName</ThemedText>
+        <ThemedView style={styles.inputContainer}>
+          <ThemedTextInput style={styles.input} onChangeText={text => onChangeText({userName: text})} id='userName' nativeID='userName' testID='userName'/>
+        </ThemedView>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
+        <ThemedText type="defaultSemiBold">Password</ThemedText>
+        <ThemedView style={styles.inputContainer}>
+          <ThemedTextInput style={styles.input} onChangeText={text => onChangeText({password: text})} id='password' nativeID='password' testID='password'/>
+        </ThemedView>
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
+      <ThemedButton style={{ backgroundColor: '#1D3D47', paddingVertical: 5, borderRadius: 3, marginTop: 20}} onPress={onSubmit}>
+        <ThemedText type="defaultSemiBold" style={{ color: "white", textAlign: 'center'}} testID='submit'>Submit</ThemedText>
+      </ThemedButton>
     </ParallaxScrollView>
   );
 }
@@ -55,16 +84,22 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     gap: 8,
+    marginBottom: 20,
+    justifyContent: "center"
   },
   stepContainer: {
     gap: 8,
     marginBottom: 8,
   },
   reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
+    height: 120,
+    width: 0,
+    bottom: 10,
     left: 0,
     position: 'absolute',
   },
+  inputContainer: { borderColor: "#ccc", borderWidth: 1, paddingVertical: 3, borderRadius: 3},
+  input: {
+    fontSize: 20
+  }
 });
